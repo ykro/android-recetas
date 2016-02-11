bc87e7ab494e96dcdf9e6691c3cfa1f08ca66f47
ykro <aacs85@gmail.com>
2016-02-11 13:56:57 -0600

    recipe main view features developed

--- app/src/main/java/edu/galileo/android/facebookrecipes/FacebookRecipesApp.java
+++ app/src/main/java/edu/galileo/android/facebookrecipes/FacebookRecipesApp.java
@@ -3,6 +3,13 @@ package edu.galileo.android.facebookrecipes;
 import android.app.Application;
 
 import com.facebook.FacebookSdk;
+import com.raizlabs.android.dbflow.config.FlowManager;
+
+import edu.galileo.android.facebookrecipes.lib.di.LibsModule;
+import edu.galileo.android.facebookrecipes.recipesmain.di.DaggerRecipesMainComponent;
+import edu.galileo.android.facebookrecipes.recipesmain.di.RecipesMainComponent;
+import edu.galileo.android.facebookrecipes.recipesmain.di.RecipesMainModule;
+import edu.galileo.android.facebookrecipes.recipesmain.ui.RecipesMainView;
 
 /**
  * Created by ykro.
@@ -12,10 +19,23 @@ public class FacebookRecipesApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        initDB();
         initFacebook();
+    }
+
+    private void initDB() {
+        FlowManager.init(this);
     }
 
     private void initFacebook() {
         FacebookSdk.sdkInitialize(getApplicationContext());
+    }
+
+    public RecipesMainComponent getRecipesMainComponent(RecipesMainView view) {
+        return DaggerRecipesMainComponent
+                .builder()
+                .libsModule(new LibsModule())
+                .recipesMainModule(new RecipesMainModule(view))
+                .build();
     }
 }
